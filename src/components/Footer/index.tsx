fae9ed6fb395b476f2a984c13a1a068c1a22c025
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-08 17:39:44 +0900

    Fix design.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import {FaGithub} from "react-icons/fa";
+import {FaNpm} from "react-icons/fa";
 
 import styles from "./styles.scss";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div><a href="https://github.com/sakai-akinobu/igata">GitHub</a></div>
+      <a className={styles.link} href="https://github.com/sakai-akinobu/igata"><FaGithub /></a>
+      <a className={styles.link} href="https://www.npmjs.com/package/igata"><FaNpm /></a>
     </footer>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import {FaGithub} from "react-icons/fa";
+import {FaNpm} from "react-icons/fa";
 
 import styles from "./styles.scss";
 
@@ -7,8 +9,12 @@ export default function Header() {
     <header className={styles.header}>
       <h1 className={styles.title}>Igata</h1>
       <nav className={styles.nav}>
-        <a className={styles.link}>GitHub</a>
-        <a className={styles.link}>npm</a>
+        <a className={styles.link} href="https://github.com/sakai-akinobu/igata">
+          <FaGithub />
+        </a>
+        <a className={styles.link} href="https://www.npmjs.com/package/igata">
+          <FaNpm />
+        </a>
       </nav>
     </header>
   );
