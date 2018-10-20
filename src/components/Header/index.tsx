5897848f21629a30de51e18e27bcdf93ee80c7ae
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-20 23:12:45 +0900

    Fix header design.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,16 +4,18 @@ import {FaNpm} from "react-icons/fa";
 
 import styles from "./styles.scss";
 
+const iconSize = 28;
+
 export default function Header() {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Igata</h1>
       <nav className={styles.nav}>
         <a className={styles.link} href="https://github.com/sakai-akinobu/igata">
-          <FaGithub />
+          <FaGithub size={iconSize} />
         </a>
         <a className={styles.link} href="https://www.npmjs.com/package/igata">
-          <FaNpm />
+          <FaNpm size={iconSize} />
         </a>
       </nav>
     </header>
